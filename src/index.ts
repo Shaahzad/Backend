ea1edd09ff33b052a005edf8bd70f6aa3da29fad
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-05-02 17:36:39 +0500

    Changes Done

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,7 @@ import userRoutes from './routes/userRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: [
-    'http://localhost:5174',
-    'http://localhost:5173',
-  ],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
